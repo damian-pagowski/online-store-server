76d4eaa843e086bd3560495a054cfd99348ac402
Damian <d.pagowski@gmail.com>
2020-01-31 02:32:10 +0700

    added route for categories. product rating changed to integer value 1-5. badges stored in product as array

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,38 @@ router.get("/", (req, res, next) => {
   res.json(productInfo);
 });
 
+router.get("/categories/", (req, res, next) => {
+  const categories = {
+    computers: {
+      display: "Computers",
+      subcategories: {
+        laptops: "Laptops",
+        tablets: "Tablets",
+        peripherials: "Peripherials",
+        accessories: "Accessories",
+      },
+    },
+    games: {
+      display: "Video Games",
+      subcategories: {
+        pc: "PC",
+        ps5: "PS 5",
+        ps4: "PS 4",
+        xbox360: "Bbox 360",
+      },
+    },
+    phones: {
+      display: "Cell Phones",
+      subcategories: {
+        smartphones: "Smart Phones",
+        wisephones: "Wise Phones",
+        hipster: "Hipster",
+        classic: "Classic",
+      },
+    },
+  };
+  res.json(categories);
+});
 router.get("/:id", (req, res, next) => {
   const id = req.params["id"];
   const found = productInfo.find(item => item.productId == id);
@@ -21,10 +53,12 @@ const productInfo = [
       in vulputate at, tempus viverra turpis. Fusce condimentum
       nunc ac nisi vulputate fringilla. Donec lacinia congue felis
       in faucibus.`,
-    rating: "8/10",
+    rating: 4,
     price: "99.99",
     productId: 1,
-    category: "game-pc",
+    category: "games",
+    subcategory: "pc",
+    badges: ["Best Seller"],
   },
   {
     name: "Y-Phone Deluxe",
@@ -33,10 +67,12 @@ const productInfo = [
       in vulputate at, tempus viverra turpis. Fusce condimentum
       nunc ac nisi vulputate fringilla. Donec lacinia congue felis
       in faucibus.`,
-    rating: "9/10",
-    category: "phone-smartphone",
+    rating: 3,
+    category: "cellphones",
+    subcategory: "smartphones",
     price: "999.99",
     productId: 2,
+    badges: ["Our Choice"],
   },
   {
     name: "Y-Book Premium Pro",
@@ -44,10 +80,12 @@ const productInfo = [
     description: `Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
         scelerisque ante sollicitudin. Cras purus odio, vestibulum
         in vulputate at, tempus viverra turpis.`,
-    rating: "9/10",
+    rating: 5,
     price: "2999.99",
-    category: "computer-laptop",
+    category: "computers",
+    subcategory: "laptops",
     productId: 3,
+    badges: ["Best Seller", "Best Value"],
   },
 ];
 module.exports = router;
