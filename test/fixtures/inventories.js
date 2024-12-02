add71eedc6709001e4b4bfbe4893dc92ce1deea2
Damian <damian.pagowski@toptal.com>
2024-12-02 05:56:23 +0100

    Increased availability of inventory in fixture

--- test/fixtures/inventories.js
+++ test/fixtures/inventories.js
@@ -1,35 +1,35 @@
 const inventories = [
     {
       productId: 1,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 2,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 3,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 4,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 5,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 6,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 7,
-      quantity: 50,
+      quantity: 99999
     },
     {
       productId: 8,
-      quantity: 50,
+      quantity: 99999
     },
   ];
   
